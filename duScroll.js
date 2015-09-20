db4308d22aa5663ea56735f31bf3cf65208cbfb8
Amruth Venkatraman <amruthv@gmail.com>
2015-09-19 20:53:25 -0400

    my initial code

--- duScroll.js
+++ duScroll.js
@@ -1,7 +1,9 @@
 var analyser = null;
-var tracks = null;
-var buflen = 1024;
-var buf = new Uint8Array( buflen );
+var FFT_SIZE = 2048;
+// var tracks = null;
+// var buflen = 1024;
+var buf;
+
 var state = [];
 
 window.onload = function() {
@@ -20,15 +22,16 @@ window.onload = function() {
                 "optional": []
             },
         }, gotStream);
-    console.log('here');
+
+    setInterval(filterOldFreqs, 1000);
+    setInterval(duScroll, 10);
 }
 
 function fillPage() {
     for (i = 0; i < 1000; i++) {
         var x = $("<p>").append("" + i);
         $("body").append(x)    
-    }
-    
+    }   
 }
 
 function error() {
@@ -56,31 +59,108 @@ function gotStream(stream) {
     mediaStreamSource = audioContext.createMediaStreamSource(stream);
     // Connect it to the destination.
     analyser = audioContext.createAnalyser();
-    analyser.fftSize = 2048;
+    analyser.fftSize = FFT_SIZE;
+    buf = new Uint8Array( analyser.frequencyBinCount );
     mediaStreamSource.connect( analyser );
-    setInterval(updatePitch, 10);
-    // updatePitch();
+    setInterval(updatePitch, 1);
 }
 
 function updatePitch() {
     analyser.getByteFrequencyData( buf );
     // console.log(buf);
-    var f = getFrequencyFromBuf();
-    console.log(f);
-    window.scrollTo(0, f*100);
-    state.push(f);
+    var currFreq = getFrequencyFromBuf();
+    currTime = Date.now();
+    // console.log(state);
+    // console.log(currFreq);
+
+    if (currFreq != -1) {
+        state.push([currFreq, currTime]);
+    }
 }
 
+function filterOldFreqs() {
+    var historyMilisecondTolerance = 4000;
+    var currTime = Date.now();
+    var i = state.length - 1;
+    while (i >= 0) {
+        var timestamp = state[i][1];
+        if (Math.abs(timestamp - currTime) > historyMilisecondTolerance) {
+            state = state.slice(i + 1, state.length);
+            return;
+            break;
+        }
+        i--;
+    }
+}
 
 function getFrequencyFromBuf() {
+    var MIN_LOUDNESS_ALLOWED = 210;
     var max = buf[0];
     var maxIndex = 0;
 
-    for (var i = 1; i < buf.length; i++) {
+    for (var i = 0; i < buf.length; i++) {
         if (buf[i] > max) {
             maxIndex = i;
             max = buf[i];
         }
     }
-    return maxIndex;
+
+    if (buf[maxIndex] > 210) {
+        return maxIndex;
+    }
+    return -1;
+}
+
+function duScroll() {
+    // var freqs = state.map(function(x){ return x[0]; });
+    // var stdev = standardDeviation(freqs);
+    // var avg = average(freqs);
+    if (state.length >= 2) {
+        console.log('here');
+        var mostRecent = state[state.length - 1][0];
+        var secondMostRecent = state[state.length - 2][0];
+        // if ((Math.abs(mostRecent - avg) <= stdev) && (Math.abs(secondMostRecent - avg) <= stdev)) {
+        // scrollBy(0,-50 * (mostRecent - secondMostRecent));
+        if (mostRecent - secondMostRecent > 0) {   
+            scrollBy(0,-50); 
+        }
+        else if (mostRecent . secondMostRecent < 0) {
+            scrollBy(0, 50);
+        }
+    }
+    // var filtered = filtervalues(state);
+
+}
+
+
+function filterValues(values) {
+    var stdev = standardDeviation(values);
+    var avg = average(values);
+    return values.filter(function(x) { return Math.abs(x[0] - avg) <= stdev; });
+}
+
+
+
+function standardDeviation(values){
+  var avg = average(values);
+  
+  var squareDiffs = values.map(function(value){
+    var diff = value - avg;
+    var sqrDiff = diff * diff;
+    return sqrDiff;
+  });
+  
+  var avgSquareDiff = average(squareDiffs);
+
+  var stdDev = Math.sqrt(avgSquareDiff);
+  return stdDev;
+}
+
+function average(data){
+  var sum = data.reduce(function(sum, value){
+    return sum + value;
+  }, 0);
+
+  var avg = sum / data.length;
+  return avg;
 }
